1169267e076b96697c22ff858292c3152701f8ed
Pierre-Charles Nicolas <A739TJ@login.axa>
2018-09-03 17:41:29 +0200

    jest setup

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,8 @@
-// import { expect } from 'chai';
-// import helloWorld from '../src/index';
+const helloWorld = () => 'Hello world';
 
-// describe('Hello World', function() {
-//   it('should work', async function() {
-//     const res = await helloWorld();
-//     expect(res).to.equal('Hello world');
-//   });
-// });
+describe('Hello World', function() {
+  it('should work', async function() {
+    const res = await helloWorld();
+    expect(res).toEqual('Hello world');
+  });
+});
